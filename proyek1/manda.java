f1db89fc4e8f502b3c2054c2805018411f4d8e3b
mandadivaa <mandadyva2005@gmail.com>
2024-08-29 15:09:36 +0700

    proyek1

---
+++ proyek1/manda.java
@@ -0,0 +1,15 @@
+public class manda {
+  public static void main(String[] args) {
+    // informasi pribadi
+    String nama = "Amanda Diva Oktaviani";
+    String npm = "2310506004";
+    String alamat = "Magelang";
+    String noHp = "085700122951";
+
+    // tampilkan informasi
+    System.out.println("Nama : " + nama);
+    System.out.println("NPM : " + npm);
+    System.out.println("Alamat : " + alamat);
+    System.out.println("No_HP : " + noHp);
+  }
+}
